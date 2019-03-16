16f77ccf3c65ba5447704a3ef927fb0e74ecab6d
Andrea Yoshida <aryoshida9340@gmail.com>
2019-03-16 11:27:01 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,133 +1,13 @@
-// // GET THE HTML TAG THAT WE WANT TO CHANGE
-// var quizBodyHTML = document.getElementById("quizBody");
+$("#start").on("click", function(){
 
-// // DEFINE THE HTML THAT WE WANT TO SWITCH TO
-// var questionHTMLText = '<div class="quizBody"></div>'
-// var multipleChoiceHTMLText = '<form action="">' +
-//     '<input type="radio" value="choice1"> <span id="choice1Text"></span><br>' +
-//     '<input type="radio" value="choice2"> <span id="choice2Text"></span><br>' +
-//     '<input type="radio" value="choice3"> <span id="choice3Text"></span>' +
-//     '</form>';
+    game.start();
 
-// // SWAP THE HTML FOR THE HTML THAT WE WANT
-// quizBodyHTML.innerHTML = questionHTMLText;
+    $("#quizBody").append("<br>");
 
-// // CREATE AN OBJECT TO REPRESENT A QUESTION
-// var questionObject = {
-//     questionText: "What is 2+2?",
-//     correctIndex: 1,
-//     choices: [ '1', '4', '5' ]
-// }
-
-// function loadQuestion(questObj) {
-//     // get question header html tag
-//     // set question headar html tag text = questObj.questionText
-//     // get choice text html tags
-//     // set choice text html tag text = questObj.choices[index]
-// }
-
-// const triviaQuestions = [
-//     {
-//         question: "What color is the sky durring the day?",
-//         choices: {
-//             a: "Yellow",
-//             b: "Purple",
-//             c: "Blue"
-//         },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "How many genders are there?",
-//         choices: {
-//             a: "2",
-//             b: "63",
-//             c: "5"
-//         },
-//         correctAnswer: "a"
-//     },
-//     {
-//         question: "What animal makes the 'moo' sound?",
-//         choices: {
-//             a: "cat",
-//             b: "cow",
-//             c: "snail"
-//         },
-//         correctAnswer: "b"
-//     }
-// ];
-
-// var questionContainer = document.getElementById("quiz");
-// var choicesContainer = document.getElementById("choices");
-// var resultsContainer = document.getElementById("results");
-// var submitButton = document.getElementById("submit");
-
-
-
-// function displayQuestions(triviaQuestions){
-
-//     var output = [];
-//     var choices;
-    
-//     for(var i = 0; i < question.length; i++){
-
-//         choices = [];
-
-//         for(letter in triviaQuestions[i].choices){
-
-//             choices.push(
-//                 '<label>'
-//                     + '<input type="radio" name="question '+i+' "value=" '+letter+' ">'
-//                     + letter + ': '
-//                     + triviaQuestions[i].choices[letter]
-//                 + '</label>'
-//             );
-//         }
-
-//         output.push(
-//             '<div class="question">' + triviaQuestions[i].question + '</div>'
-//             + '<div class="answers">' + choices.join('') + '</div>'
-//         );
-
-//     }
-
-//     quizHolder.innerHTML = output.join('');
-
-// }
-
-// displayQuestions(questionContainer);
-
-
-// function showResults(triviaQuestions, quizHolder, resultsHolder){
-
-//     var answerHolder = quizHolder.querySelectorAll(".correctAnswer");
-//     var userChoice = '';
-//     var correct = 0;
-//     var incorrect = 0;
-
-//     for(var i = 0; i , question.length; i ++){
-//         userChoice = (answerHolder[i].querySelector("input[name=question")||{}).value;
-
-//         if(userChoice === triviaQuestions[i].correctAnswer){
-//             correct++;
-//             answerHolder[i].style.color = "lightgreen";
-//         } else{
-//             answerHolder[i].style.color = "red";
-//             incorrect++;
-//         }
-//     }
-
-//     resultsHolder.innerHTML = correct + ' out of ' + question.length;
-// }
-
-// submitButton.onclick = function(){
-//     showResults(triviaQuestions, quizHolder, resultsHolder);
-// }
-
-    
-// displayQuestions(triviaQuestions, quizHolder);
-
-// createQuiz(triviaQuestions, quizHolder, resultsHolder, submitButton);
-
+    $(document).on("click", "#end", function(){
+        game.done();
+    })
+});
 
 const triviaQuestions = [
     {
@@ -163,7 +43,7 @@ const triviaQuestions = [
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 60,
+    counter: 30,
     countdown: function(){
         game.counter--;
         $("#counter").html(game.counter);
@@ -187,26 +67,55 @@ var game = {
                 $("#quizBody").append("<input type='radio' name='question-"+i+"' value='"+triviaQuestions[i].choices[j]+"'>"+triviaQuestions[i].choices[j]);
             }
 
-        }        
+        } 
+        
+        $("#quizBody").append("<br><button id='end'>Submit</button>");
     },
     done: function(){
-        $.each($("input[name='question-1]':checked"))
+        $.each($("input[name='question-0]':checked"),function(){
+            if($(this).val()==triviaQuestions[0].correctAnswer){
+                game.correct++;
+            } else{
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-1]':checked"),function(){
+            if($(this).val()==triviaQuestions[1].correctAnswer){
+                game.correct++;
+            } else{
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-2]':checked"),function(){
+            if($(this).val()==triviaQuestions[2].correctAnswer){
+                game.correct++;
+            } else{
+                game.incorrect++;
+            }
+        });
+        $.each($("input[name='question-3]':checked"),function(){
+            if($(this).val()==triviaQuestions[3].correctAnswer){
+                game.correct++;
+            } else{
+                game.incorrect++;
+            }
+        });             
+
+        this.results();
+
+    },
+
+    results: function(){
+        clearInterval(timer);
+        $("#quizBody h2").remove();
+
+        $("quizbody").html("<h2>Done!</h2>");
+        $("#quizbody").append("<h3>Correct Answers: " + this.correct + "</h3>");
+        $("#quizbody").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+        $("#quizbody").append("<h3>Unanswered: " + (triviaQuestions.length-(this.incorrect + this.correct)) + "</h3>");
+
     }
 }
-
-
-
-$("#start").on("click", function(){
-
-    game.start();
-
-    $("#quizBody").append("<br>");
-
-    $("#quizBody").append("<button id='submit'>Submit</button>");
-
-});
-
-
 
 // $('#Submit').on('click', ...);
 
